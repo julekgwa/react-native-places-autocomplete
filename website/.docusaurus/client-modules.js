4c642fa92dfbe7676afe763ba7701d88eb0f33d8
Junius Lekgwara <phutigravel@gmail.com>
2025-09-28 15:25:59 +0200

    chore: removed .docusaurus from version contrl

--- website/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/private/tmp/react-native-places-autocomplete/website/node_modules/infima/dist/css/default/default.css"),
-  require("/private/tmp/react-native-places-autocomplete/website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/private/tmp/react-native-places-autocomplete/website/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/private/tmp/react-native-places-autocomplete/website/src/css/custom.css"),
-];
